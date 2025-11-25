0888b5b9707684ac9be357649aabf2b91a108fa6
Sreelatha <sreelatha2204@gmail.com>
2025-11-25 21:23:52 +0530

    dataset

---
+++ Policepatrol proj/main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Data loading
+balance_df = pd.read_excel('C:/Users/sreelatha/code/guvi-projects/Policepatrol proj/Crimes_-_2001_to_Present_20251125.csv')
